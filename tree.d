985b3cdec4acce0d514566b4d7553add5a79e236
Kohe Tokoi <tokoik@gmail.com>
2012-06-26 23:47:39 +0900

    update dependency

--- tree.d
+++ tree.d
@@ -2,4 +2,6 @@ Matrix.o: Matrix.cpp Matrix.h
 Trackball.o: Trackball.cpp Trackball.h
 Tree.o: Tree.cpp extrusion.h Matrix.h Tree.h
 extrusion.o: extrusion.cpp extrusion.h
-main.o: main.cpp Trackball.h Tree.h Matrix.h
+main.o: main.cpp /usr/X11R6/include/GL/glut.h /usr/X11R6/include/GL/gl.h \
+  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h Trackball.h \
+  Tree.h Matrix.h
